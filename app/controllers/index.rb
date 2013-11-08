4a808aaf17dfa85d9eedea907fc9c5569c0fa18f
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-08 15:55:15 -0600

    bcrypt and login stuff

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,20 +5,36 @@ get '/' do
   erb :index
 end
 
-
-get '/users/:user_id/surveys' do
-end
-
-
 get '/login' do
+
+  erb :login
 end
-
-### POST ###
-
-post '/users/new' do
-
-end
-
 
 post '/login' do
 end
+
+# create new user
+post '/users/new' do
+  if User.find_by_username(params[:username]) #if username is in the database, pick another
+    return "Username taken!"
+  else
+    if params[:password] == params[:confirmed_password]
+      user = User.create(username: params[:username]) # make user
+      session[:user_id] = user.id # set session equal to user_id
+      user.password = params[:password] # creates encrypted password
+      user.save
+    else
+      return "Password does not match. Please try again."
+    end
+  end
+end
+
+
+get '/users/:user_id/surveys' do
+
+erb :profile
+end
+
+
+
+
