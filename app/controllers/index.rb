817df06a5e64e75b54f94966d96660919aa09020
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-10 17:30:13 -0600

    Added completed surveys to profile page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -40,8 +40,20 @@ end
 
 get '/users/:user_id/surveys' do
   if session[:user_id] != nil
-    @surveys = Survey.where(:user_id => params[:user_id])
+    @surveys_created = Survey.where(:user_id => params[:user_id])
+    
     @user = User.find(params[:user_id])
+    surveys_completed = Complete.where(:user_id => params[:user_id])
+    @completed_survey_titles = []
+    
+    surveys_completed.each do |complete|
+      @completed_survey_titles << Survey.find(complete.survey_id).title
+    end
+
+  p @completed_survey_titles
+
+
+
     erb :profile
   else
     redirect to '/'
